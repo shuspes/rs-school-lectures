b482819e02ebb8f2a75907d1dc879cabd307a4ac
shuspes <shuspes@gmail.com>
2019-01-02 17:12:26 +0300

    add mocha tests

---
+++ user-list/src/details/details.test.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import Details from './details';
+
+describe('Details class', () => {
+  it('constructor takes 3 params', () => {
+    assert.ok(Details.length === 3);
+  });
+});
